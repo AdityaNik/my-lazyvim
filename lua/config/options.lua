10eda90ecc995324933bc59252e5a30e3ee4efff
ditya <105343290+AdityaNik@users.noreply.github.com>
2025-08-30 04:00:19 +0530

    web config done

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,26 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.mapleader = " "
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.backup = false
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 0
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.scrolloff = 10
+vim.opt.inccommand = "split"
+vim.opt.wrap = false
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" })
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitkeep = "cursor"
